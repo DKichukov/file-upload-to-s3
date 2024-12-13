d501494880d6e08f3e77e2d5415672d276faa260
DKichukov <dzhamal.kichukov@gmail.com>
2024-12-13 15:07:24 +0200

    Initial commit

---
+++ src/main/java/com/example/file_upload_to_s3/FileUploadToS3Application.java
@@ -0,0 +1,13 @@
+package com.example.file_upload_to_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadToS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadToS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/file_upload_to_s3/FileUploadToS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_upload_to_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadToS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
